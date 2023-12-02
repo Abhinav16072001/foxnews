bd594f7f1cc432fe238ea835bcb2da1f5ae418ce
Abhinav <66725911+Abhinav16072001@users.noreply.github.com>
2023-12-02 16:58:07 +0530

    video 27

--- src/components/News.js
+++ src/components/News.js
@@ -1,71 +1,22 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import NewsItem from "./NewsItem";
 
 export default class News extends Component {
-  static propTypes = {
-    prop: PropTypes.func,
-  };
-
-  articles = [
-    {
-      source: {
-        id: null,
-        name: "TMZ",
-      },
-      author: "TMZ Staff",
-      title: "'Euphoria' Producer Kevin Turen Died From Acute Heart Failure",
-      description:
-        'Kevin Turen, famous for producing HBO shows like "Euphoria" and "The Idol," died as a result of multiple heart issues ... TMZ has learned. The producer\'s official cause of death is acute cardiac dysfunction and hypertrophic heart disease ...…\n Permalink',
-      url: "https://www.tmz.com/2023/12/02/euphoria-producer-kevin-turen-cause-of-death-heart-failure-disease/",
-      urlToImage:
-        "https://imagez.tmz.com/image/3a/16by9/2023/12/01/3aed77f231854d16a79009b32610380c_xl.jpg",
-      publishedAt: "2023-12-02T08:50:57Z",
-      content:
-        'Kevin Turen, famous for producing HBO shows like "Euphoria" and "The Idol," died as a result of multiple heart issues ... TMZ has learned.\r\nThe producer\'s official cause of death is acute cardiac dys… [+758 chars]',
-    },
-    {
-      source: {
-        id: null,
-        name: "Biztoc.com",
-      },
-      author: "youtube.com",
-      title:
-        "Raoul Pal: Coinbase, Tesla, and Solana Hitting it Big #shorts #solana #tesla #coinbase #tech",
-      description:
-        "GET 3 months of Real Vision for Just $49 https://rvtv.io/463wkrF Elevate your brand with Real Vision. Connect with us at ... #realvision",
-      url: "https://biztoc.com/x/7992715c96d02bc9",
-      urlToImage: "https://c.biztoc.com/p/7992715c96d02bc9/s.webp",
-      publishedAt: "2023-12-02T08:06:20Z",
-      content:
-        "GET 3 months of Real Vision for Just $49 https://rvtv.io/463wkrF Elevate your brand with Real Vision. Connect with us at ...\r\n#realvision\r\nThis story appeared on youtube.com, .",
-    },
-    {
-      source: {
-        id: null,
-        name: "Whitecoatinvestor.com",
-      },
-      author: "Lauren O'Brien",
-      title:
-        "A Financial Love Letter to My Wife (and the Realities of Living Like a Resident)",
-      description:
-        "I write a lot about how personal finance applies to my life and profession. But how does living like a resident affect my marriage?\nThe post A Financial Love Letter to My Wife (and the Realities of Living Like a Resident) appeared first on The White Coat Inve…",
-      url: "https://www.whitecoatinvestor.com/realities-of-living-like-a-resident/",
-      urlToImage:
-      "https://images.wsj.net/im-893490/social",
-      publishedAt: "2023-12-02T07:30:37Z",
-      content:
-        "By The Motivated MD, Guest Writer\r\nThere are times in our lives that force us to stop and reflect. If I am being completely honest, I am a very self-reflective person. Recently, I found myself thinki… [+18300 chars]",
-    },
-  ];
-
   constructor() {
     super();
-    console.log("OK");
+
     this.state = {
-      articles: this.articles,
+      articles: [],
       loading: false,
     };
+  }
+
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=0529b7bd50dd4e6596cbf39ab52de290";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({ articles: parsedData.articles });
   }
 
   render() {
@@ -74,14 +25,24 @@ export default class News extends Component {
         <h2>Top Headings</h2>
         <div className="row">
           {this.state.articles.map((element) => {
-            return <div className="col-md-4" key={element.url}>
-              <NewsItem
-                title={element.title.slice(0, 40)}
-                description={element.description.slice(0, 88)}
-                newsUrl={element.url}
-                img={element.urlToImage}
-              />
-            </div>;
+            return (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title.slice(0, 40) : ""}
+                  description={
+                    element.description
+                      ? element.description.slice(0, 88)
+                      : "Nothing to show"
+                  }
+                  newsUrl={element.url}
+                  img={
+                    element.urlToImage
+                      ? element.urlToImage
+                      : "https://www.livelaw.in/h-upload/2023/10/31/501020-tamil-nadu-government-questioning-governors-slow-pace-in-in-assenting-bills-proclaiming-him-as-a-political-rival.jpg"
+                  }
+                />
+              </div>
+            );
           })}
         </div>
       </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,14 +5,14 @@ export class NewsItem extends Component {
     let { title, description, img, newsUrl } = this.props;
 
     return (
-      <div>
+      <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
           <img src={img} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
             <a href={`${newsUrl}`} target="_" className="btn btn-primary">
-              Go somewhere
+              Read more
             </a>
           </div>
         </div>
