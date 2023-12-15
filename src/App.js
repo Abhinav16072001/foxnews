5ffb1311416ac78074f34be049fa3c90035f5924
Abhinav <66725911+Abhinav16072001@users.noreply.github.com>
2023-12-15 23:40:29 +0530

    video 37

--- src/App.js
+++ src/App.js
@@ -7,16 +7,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoadingBar from "react-top-loading-bar";
 
 export default class App extends Component {
+  apiKey = process.env.REACT_APP_NEWS_API_KEY;
+
   state = {
     progress: 0,
   };
 
-  setProgress = (progress) =>{
-      this.setState({
-        progress: progress
-      })
-  }
-
+  setProgress = (progress) => {
+    this.setState({
+      progress: progress,
+    });
+  };
 
   render() {
     return (
@@ -28,27 +29,62 @@ export default class App extends Component {
           <Route
             exact
             path="/"
-            element={<News setProgess={this.setProgress} key="general" category="general" />}
+            element={
+              <News
+                setProgess={this.setProgress}
+                apiKey={this.apiKey}
+                key="general"
+                category="general"
+              />
+            }
           />
           <Route
             exact
             path="/science"
-            element={<News setProgess={this.setProgress} key="science" category="science" />}
+            element={
+              <News
+                setProgess={this.setProgress}
+                apiKey={this.apiKey}
+                key="science"
+                category="science"
+              />
+            }
           />
           <Route
             exact
             path="/health"
-            element={<News setProgess={this.setProgress} key="health" category="health" />}
+            element={
+              <News
+                setProgess={this.setProgress}
+                apiKey={this.apiKey}
+                key="health"
+                category="health"
+              />
+            }
           />
           <Route
             exact
             path="/business"
-            element={<News setProgess={this.setProgress} key="business" category="business" />}
+            element={
+              <News
+                setProgess={this.setProgress}
+                apiKey={this.apiKey}
+                key="business"
+                category="business"
+              />
+            }
           />
           <Route
             exact
             path="/sports"
-            element={<News setProgess={this.setProgress} key="sports" category="sports" />}
+            element={
+              <News
+                setProgess={this.setProgress}
+                apiKey={this.apiKey}
+                key="sports"
+                category="sports"
+              />
+            }
           />
           <Route exact path="/about" element={<h1>None</h1>} />
         </Routes>
